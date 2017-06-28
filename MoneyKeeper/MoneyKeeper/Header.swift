87eb4693f110a267667edec10520c9c4400aee70
Tran Chung <tranchung927@gmail.com>
2017-06-28 10:13:09 +0700

    Cell and header Categories

---
+++ MoneyKeeper/MoneyKeeper/Header.swift
@@ -0,0 +1,24 @@
+//
+//  Header.swift
+//  MoneyKeeper
+//
+//  Created by Tran Chung on 6/28/17.
+//  Copyright © 2017 Tran Chung. All rights reserved.
+//
+
+import UIKit
+
+class Header: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

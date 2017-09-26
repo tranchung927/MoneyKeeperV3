9451f9e2cdb69b0d76190711557c62536d2dffa4
Chung Tran <tranchung927@gmail.com>
2017-09-26 11:14:13 +0700

    Design TabView

--- MoneyKeeper/MoneyKeeper/ExtentionView.swift
+++ MoneyKeeper/MoneyKeeper/ExtentionView.swift
@@ -20,3 +20,90 @@ extension UIView {
         self.layer.add(animation, forKey: nil)
     }
 }
+
+fileprivate class Keys {
+    static let TOP_BORDER = "top-border"
+    static let TOP_BORDER_VIEW = "top-border-view"
+}
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            if newValue == -1 {
+                self.clipsToBounds = true
+                self.layer.cornerRadius = self.bounds.width < self.bounds.height ? self.bounds.width * 0.5 : self.bounds.height * 0.5
+            } else {
+                layer.masksToBounds = true
+            }
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var topBorder: Bool {
+        get {
+            if self.layer.value(forKey: Keys.TOP_BORDER) != nil {
+                return self.layer.value(forKey: Keys.TOP_BORDER) as! Bool
+            }
+            
+            return false
+        }
+        set {
+            self.layer.setValue(newValue, forKey: Keys.TOP_BORDER)
+        }
+    }
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+}
